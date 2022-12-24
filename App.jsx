4ac5542a409e7c4556562b4128723f723549297e
aesashi <k.for-nba@live.jp>
2022-12-23 18:45:59 -0600

    add Hello component

--- App.jsx
+++ App.jsx
@@ -2,9 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Hello from './src/components/Hello';
+
 export default function App() {
   return (
     <View style={styles.container}>
+      <Hello bang style={{ fontSize: 16 }}>World</Hello>
+      <Hello >World</Hello>
       <Text>Open up App.js to start working on your app!</Text>
       {/* eslint-disable */}
       <StatusBar style="auto" />
