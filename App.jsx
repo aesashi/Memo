7d2a51abe15a60575210832fc1871d9a7ca4267c
aesashi <k.for-nba@live.jp>
2022-12-24 15:03:31 -0600

    Installed     @react-navigation/native: ^5.8.10,     @react-navigation/stack: ^5.12.8, Error Fixing' '

--- App.jsx
+++ App.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import LogInScreen from './src/screens/LogInScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+// import LogInScreen from './src/screens/LogInScreen';
 // import SignUpScreen from './src/screens/SignUpScreen';
 // import MemoCreateScreen from './src/screens/MemoCreateScreen';
 // import MemoEditScreen from './src/screens/MemoEditScreen';
 import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
 
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
-    <MemoListScreen />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={MemoListScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
