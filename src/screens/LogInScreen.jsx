70aa8ed41703aae9e041dce295540fe2cd1ae908
aesashi <k.for-nba@live.jp>
2022-12-26 00:07:32 -0600

    Auto login and logout button

--- src/screens/LogInScreen.jsx
+++ src/screens/LogInScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   View, Text, TextInput, StyleSheet, TouchableOpacity, Alert,
 } from 'react-native';
@@ -11,10 +11,22 @@ export default function LogInScreen(props) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        navigation.reset({
+          index: 0,
+          routes: [{ name: 'MemoList' }],
+        });
+      }
+    });
+    return unsubscribe;
+  }, []);
+
   function handlePress() {
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then((userCredential) => {
-        const {user} = userCredential;
+        const { user } = userCredential;
         console.log(user.uid);
         navigation.reset({
           index: 0,
@@ -49,6 +61,7 @@ export default function LogInScreen(props) {
         />
         <Button
           label="Submit"
+          // eslint-disable-next-line react/jsx-no-bind
           onPress={handlePress}
         />
         <View style={styles.footer}>
