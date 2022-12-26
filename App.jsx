0ee4b586dbce9897e7d8e9127e597f22849f2ec7
aesashi <k.for-nba@live.jp>
2022-12-25 18:12:49 -0600

    implement sigup and login

--- App.jsx
+++ App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { CardStyleInterpolators, createStackNavigator } from '@react-navigation/stack';
+import firebase from 'firebase';
 
 import LogInScreen from './src/screens/LogInScreen';
 import SignUpScreen from './src/screens/SignUpScreen';
@@ -9,8 +10,13 @@ import MemoEditScreen from './src/screens/MemoEditScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
 import MemoListScreen from './src/screens/MemoListScreen';
 
+import { firebaseConfig } from './env';
+
 const Stack = createStackNavigator();
 
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
 export default function App() {
   return (
     <NavigationContainer>
